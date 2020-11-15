c8ba71b5f115e2356bab24f37f0e95792880d6f3
Samantha Chin <chinsamanthaashley@gmail.com>
2020-11-15 08:50:08 -0500

    HTML edited

--- world.php
+++ world.php
@@ -11,10 +11,30 @@ $country= filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
+<table>
+    <thead>
+        <tr>
+            <th>Country Name</th>
+            <th>Continent</th>
+            <th>Independence Year</th>
+            <th>Head of State</th>
+        </tr>
+    </thead>
+  <tbody>
+  <?php foreach ($results as $row):?>
+      <tr>
+          <td><?= $row['name']; ?></td>
+          <td><?= $row['continent']; ?></td>
+          <hd><?= $row['independence_year']; ?></td>
+          <td><?=  $row['head_of_state'];?></td>
+      <tr>
+  <?php endforeach;?>
+  </tbody>
+</table>
+
+<--!ul>
+<?php /*foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<?php endforeach; */?>
+</ul-->
